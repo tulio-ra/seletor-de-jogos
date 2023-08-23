0c6111f89dd5ca217f221829b10f21e61afcad47
tulio-ra <141747254+tulio-ra@users.noreply.github.com>
2023-08-23 18:38:06 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+Swal.fire({
+    title: 'Update pendente',
+    text: 'Versões reformuladas dos jogos serão lançadas em breve.',
+    icon: 'info',
+    confirmButtonColor: '#3085d6',
+    imageAlt: 'Custom image',
+})
+
 $(document).on('click', '.nav-link.warning', function(e){
     e.preventDefault();
     Swal.fire({
@@ -8,7 +16,7 @@ $(document).on('click', '.nav-link.warning', function(e){
         cancelButtonText: 'Voltar',
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Continuar!'
+        confirmButtonText: 'Continuar'
     }).then((result) => {
         if (result.isConfirmed) {
             $(document).attr('location', $(this).attr('href'));
@@ -18,9 +26,20 @@ $(document).on('click', '.nav-link.warning', function(e){
 
 $(document).on('click', '.easter-egg', function(e){
     Swal.fire({
-        title: 'Um desenvolvedor web!',
+        title: 'Um desenvolvedor web',
         text: 'Estudante do 2° ano de informática do IFMG Campus Formiga.',
         icon: 'info',
+        confirmButtonColor: '#3085d6',
+        imageAlt: 'Custom image',
+    })
+});
+
+$(document).on('click', 'a[href="#!"]', function(e){
+    Swal.fire({
+        title: 'Este jogo está em manutenção',
+        text: 'Volte mais tarde.',
+        confirmButtonColor: '#3085d6',
+        icon: 'error',
         imageAlt: 'Custom image',
     })
 });
